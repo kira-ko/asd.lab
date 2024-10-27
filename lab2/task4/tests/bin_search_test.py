5f3e8b055b066087ad5e94efc2654a12502b98b8
kira-ko <kirrzi.k@gmail.com>
2024-10-28 02:38:13 +0300

    [feat]: added task1 in lab2 [feat]: added task3 in lab2 [feat]: added task5 in lab2 [feat]: added task4 in lab2

---
+++ lab2/task4/tests/bin_search_test.py
@@ -0,0 +1,11 @@
+import unittest
+from lab2.task4.src.bin_search import binary_search
+
+class TestBinarySearch(unittest.TestCase):
+    def test_binarysearch1(self):
+        self.assertEqual(binary_search([1, 2, 3, 4, 5], 3), 2)
+        self.assertEqual(binary_search([1, 2, 3, 4, 5], 8), -1)
+
+    def test_binarysearch2(self):
+        self.assertEqual(binary_search([], 1), -1)
+
