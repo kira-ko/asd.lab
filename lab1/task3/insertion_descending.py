670c8c4d77f1ba8a80f89efadfb880890648e2e3
kira-ko <kirrzi.k@gmail.com>
2024-10-27 16:02:56 +0300

    [feat]: added task1 in lab1 [feat]: added task3 in lab1

---
+++ lab1/task3/insertion_descending.py
@@ -0,0 +1,17 @@
+def insertion_descending(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key > arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+    return arr
+
+
+if __name__ == '__main__':
+    with open('input.txt') as f:
+        n, mas = f.readlines()
+    array = insertion_descending(list(map(int, mas.split())))
+    with open('output.txt', 'w') as f:
+        print(' '.join(list(map(str, array))), file=f)
